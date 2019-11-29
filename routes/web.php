83d965284273905405c66d7c1378730f09506b81
Anish Hajare <anish.hajare9@gmail.com>
2019-11-29 12:48:52 +0530

    Registration feature implemented

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,15 @@
 
 use Faker\Provider\Lorem;
 
-Route::get('/', 'PostsController@index');
+Route::get('/', 'PostsController@index')->name('home');
 Route::post('/posts', 'PostsController@store');
 Route::get('/posts/create', 'PostsController@create');
 Route::get('/posts/{post}', 'PostsController@show');
 
 Route::post("/posts/{post}/comments", 'CommentsController@store');
+
+Route::get("/register",'RegistrationController@create');
+Route::post("/register",'RegistrationController@store');
+
+Route::get("/login",'SessionsController@create');
+Route::get("/logout",'SessionsController@destroy');
